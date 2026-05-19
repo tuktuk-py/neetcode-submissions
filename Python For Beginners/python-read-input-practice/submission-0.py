9794639c2db475679f71a347043e12692f375f9e
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-05-19 16:52:17 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    numbers = input().split(",")
+    number1 = int(numbers[0])
+    number2 = int(numbers[1])
+    return number1 + number2
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
