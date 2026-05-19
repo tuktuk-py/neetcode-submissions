da7fad6318358d69d863592d8663dacb670f4638
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-05-19 14:17:56 -0400

    Add: python-list-find - submission-0

---
+++ Python For Beginners/python-list-find/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List # this is used to add type hints for List type
+
+def find_index(nums: List[int], target: int) -> int:
+    if target in nums:
+        return(nums.index(target))
+
+
+# don't modify code below this line
+print(find_index([1, 2, 3, 4, 5], 3))
+print(find_index([1, 2, 3, 4, 5, 3], 3))
+print(find_index([1, 2, 3, 4], 1))
+print(find_index([1, 3, 4, 2], 2))
+
