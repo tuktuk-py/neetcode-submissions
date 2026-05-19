182d90179bdea01047f30314a51de32e2aa1d7ff
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-05-19 13:22:39 -0400

    Add: python-string-looping-shorthand - submission-1

---
+++ Python For Beginners/python-string-looping-shorthand/submission-1.py
@@ -0,0 +1,9 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    for i in word1:
+        print(i)
+    for j in word2:
+        print(j)
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
