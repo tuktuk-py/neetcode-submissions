f0ace652a361ad6b66fc02e500810410234cadf9
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-05-19 15:01:17 -0400

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,13 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+d = your_dict.get("d")
+
+print(your_dict)
+print(your_dict["a"])
+print("d" in your_dict)
+your_dict["a"] = 4
+print(your_dict)
