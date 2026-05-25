03556008e5a6ecb45307f14604ceb7451e66581e
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-05-24 22:16:03 -0400

    Add: longest-common-prefix - submission-5

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:    
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for s in strs[1:]:
+            j = 0
+            while j < min(len(prefix),len(s)) and prefix[j] == s[j]:
+                j+=1
+            prefix = prefix[:j]
+        return prefix
