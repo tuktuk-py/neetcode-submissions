8a399f496c4294384b240085085525e4a499fc99
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-03 18:38:26 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid = {")":"(","}":"{","]":"["}
+        stack = []
+        for i in s:
+            if i in valid:
+                if stack and stack[-1] == valid[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
