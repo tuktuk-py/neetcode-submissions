15d25af901bde779854fc0f6e1acfc89ecf485a5
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-20 17:46:23 -0400

    Update: sql-default-values - submission-1

--- SQL/sql-default-values/submission-1.sql
+++ SQL/sql-default-values/submission-1.sql
@@ -1,23 +1,24 @@
-CREATE TABLE cities (
-    name TEXT,
-    population INTEGER,
-    country TEXT,
-    is_capital BOOLEAN,
-    founded_date DATE
+CREATE TABLE videos (
+    id INTEGER,
+    name TEXT DEFAULT'Untitled',
+    is_published BOOLEAN DEFAULT false
 );
 
 
 
--- Do not modify below this line --
-INSERT INTO cities (name, population, country, is_capital, founded_date) 
-VALUES ('New York', 8175133, 'United States', FALSE, '1624-01-01'),
-        ('Beijing', 21516000, 'China', TRUE, '1045-01-01'),
-        ('Paris', 2243833, 'France', TRUE, '0259-01-01'),
-        ('London', 8136000, 'United Kingdom', TRUE, '0043-01-01'),
-        ('New Delhi', 14200467, 'India', TRUE, '1911-01-01'),
-        ('Mexico City', 8851080, 'Mexico', TRUE, '1325-01-01'),
-        ('Sao Paulo', 11316149, 'Brazil', FALSE, '1554-01-01'),
-        ('Jakarta', 10075310, 'Indonesia', TRUE, '1527-01-01'),
-        ('Karachi', 14910352, 'Pakistan', FALSE, '1729-01-01');
 
-SELECT * FROM cities;
+
+
+-- Do not modify below this line --
+INSERT INTO videos (id, name, is_published) 
+VALUES (1, 'My Video', true),
+       (2, 'Another Video', false);
+
+INSERT INTO videos (id)
+VALUES (3),
+       (4);
+
+INSERT INTO videos (name)
+VALUES ('Video with no ID');
+
+SELECT * FROM videos;
