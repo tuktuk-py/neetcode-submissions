de82d48158dbe298c32814bdd079ab51eb5ad12f
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-20 18:24:19 -0400

    Update: sql-arrays - submission-1

--- SQL/sql-arrays/submission-1.sql
+++ SQL/sql-arrays/submission-1.sql
@@ -1,8 +1,7 @@
-CREATE TABLE events (
+CREATE TABLE orders (
     id INTEGER PRIMARY KEY,
-    event_date DATE,
-    event_time TIME,
-    event_timestamp TIMESTAMP
+    items TEXT[],
+    total_price INTEGER
 );
 
 
@@ -11,13 +10,9 @@ CREATE TABLE events (
 
 
 -- Do not modify below this line --
-INSERT INTO events (id, event_date, event_time, event_timestamp) 
-  VALUES (1, '2000-01-01', '12:30:10', '2000-01-01 12:30:10');
+INSERT INTO orders (id, items, total_price) 
+    VALUES (1, ARRAY['apple', 'banana'], 100),
+          (2, ARRAY['orange', 'grape'], 200),
+          (3, ARRAY['watermelon', 'pineapple'], 300);
 
-SELECT 
-    e.*,
-    (SELECT STRING_AGG(column_name || ' ' || data_type, ', ')
-     FROM information_schema.columns 
-     WHERE table_name = 'events') AS column_types
-FROM 
-    events e;
+SELECT * FROM orders;
