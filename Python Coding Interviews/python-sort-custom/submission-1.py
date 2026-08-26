a81759d5f56fbe034334053ebf40b7786cb93de5
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-26 19:55:24 -0400

    Add: python-sort-custom - submission-1

---
+++ Python Coding Interviews/python-sort-custom/submission-1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def length_word(words):
+    return len(words)
+
+def absoulte(numbers):
+    return abs(numbers)
+
+def sort_words(words: List[str]) -> List[str]:
+    words.sort(key=length_word,reverse=True)
+    return words
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    numbers.sort(key=absoulte)
+    return numbers
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
