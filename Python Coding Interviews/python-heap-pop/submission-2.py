7f56d2de1134e7f83e24b562345fdcb3ec32b82e
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-27 13:49:58 -0400

    Add: python-heap-pop - submission-2

---
+++ Python Coding Interviews/python-heap-pop/submission-2.py
@@ -0,0 +1,14 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    ans = []
+    while heap:
+        ans.append(heapq.heappop(heap))
+    return ans
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
