f1b6ba35b2c87be62870719a160d2c5e7b819dfb
tuktuk-py <126955050+tuktuk-py@users.noreply.github.com>
2026-08-27 13:58:08 -0400

    Add: python-max-heap - submission-2

---
+++ Python Coding Interviews/python-max-heap/submission-2.py
@@ -0,0 +1,21 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    max_heap = []
+    for n in nums:
+        heapq.heappush(max_heap,-n)
+    ans = []
+    while max_heap:
+        ans.append(-heapq.heappop(max_heap))
+    return ans
+
+
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
